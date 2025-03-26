6e1d494c62f0ce2aad29dcdde149b4abb14fb437
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-26 17:47:06 -0500

    configure services for database context, repositories, and controllers

--- AttendanceSystem/Program.cs
+++ AttendanceSystem/Program.cs
@@ -1,6 +1,24 @@
 using AttendanceSystem.Components;
+using AttendanceSystem.Data;
+using AttendanceSystem.Data.Repositories;
+using AttendanceSystem.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add database context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add professors repository
+builder.Services.AddScoped<IProfessorRepository, ProfessorRepository>();
+
+// Add professors service
+builder.Services.AddScoped<IProfessorService, ProfessorService>();
+
+// Add professors controller
+builder.Services.AddControllers();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -16,6 +34,8 @@ if (!app.Environment.IsDevelopment())
 
 
 app.UseAntiforgery();
+
+app.MapControllers();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
