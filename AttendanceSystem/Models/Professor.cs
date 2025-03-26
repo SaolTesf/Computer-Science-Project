e7997da2576fc6c2618143f4f324f27d9b56f6a0
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-26 16:54:37 -0500

    rename UserName property to Username in Professor model

--- AttendanceSystem/Models/Professor.cs
+++ AttendanceSystem/Models/Professor.cs
@@ -4,7 +4,7 @@ public class Professor {
   public string ID { get; set; } = null!;
   public string FirstName { get; set; } = null!;
   public string LastName { get; set; } = null!;
-  public string UserName { get; set; } = null!;
+  public string Username { get; set; } = null!;
   public string Email { get; set; } = null!;
   public string PasswordHash { get; set; } = null!;
 
