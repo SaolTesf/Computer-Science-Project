c91756eca8e18c5b8b511df863e87dc77ba4e516
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-26 17:32:48 -0500

    add ProfessorsController for managing professor-related API endpoints

---
+++ AttendanceSystem/Controllers/ProfessorsController.cs
@@ -0,0 +1,10 @@
+using AttendanceSystem.Models;
+using AttendanceSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceSystem.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ProfessorsController: ControllerBase {
+  }
+}
