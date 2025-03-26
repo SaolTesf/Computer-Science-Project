15241d7b95b6d5e858d224670819f30ddf22a03a
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-26 16:52:52 -0500

    add Professor model for Attendance System

---
+++ AttendanceSystem/Models/Professor.cs
@@ -0,0 +1,13 @@
+namespace AttendanceSystem.Models;
+
+public class Professor {
+  public string ID { get; set; } = null!;
+  public string FirstName { get; set; } = null!;
+  public string LastName { get; set; } = null!;
+  public string UserName { get; set; } = null!;
+  public string Email { get; set; } = null!;
+  public string PasswordHash { get; set; } = null!;
+
+  //public virtual ICollection<Course> Courses { get; set; } = new List<Course>();
+
+}
