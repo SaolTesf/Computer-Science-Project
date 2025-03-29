c3513b1085f1e9e0b206917745d18c25b9d33a1b
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-29 18:52:03 -0500

    add methods to IProfessorRepository for CRUD operations and existence checks

--- AttendanceSystem/Data/Repositories/IProfessorRepository.cs
+++ AttendanceSystem/Data/Repositories/IProfessorRepository.cs
@@ -3,5 +3,14 @@ using AttendanceSystem.Models;
 namespace AttendanceSystem.Data.Repositories;
 
 public interface IProfessorRepository {
-
+  Task<IEnumerable<Professor>> GetAllProfessorsAsync();
+  Task<Professor?> GetProfessorByIdAsync(String id);
+  Task<Professor?> GetProfessorByUsernameAsync(String username);
+  Task<Professor?> GetProfessorByEmailAsync(String email);
+  Task AddProfessorAsync(Professor professor);
+  Task UpdateProfessorAsync(Professor professor);
+  Task DeleteProfessorAsync(Professor professor);
+  Task<bool> ProfessorExistsAsync(String id);
+  Task<bool> ProfessorExistsByUsernameAsync(String username);
+  Task<bool> ProfessorExistsByEmailAsync(String email);
 }
