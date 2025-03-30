a1ee6b1f0a50549ad22a74197b5ae9c0960c600f
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-30 15:45:48 -0500

    add methods to IProfessorService for managing professors and existence checks

--- AttendanceSystem/Services/IProfessorService.cs
+++ AttendanceSystem/Services/IProfessorService.cs
@@ -3,5 +3,14 @@ using AttendanceSystem.Models;
 namespace AttendanceSystem.Services;
 
 public interface IProfessorService {
-
+  Task<IEnumerable<Professor>> GetAllProfessorsAsync();
+  Task<Professor?> GetProfessorByIdAsync(String id);
+  Task<Professor?> GetProfessorByUsernameAsync(String username);
+  Task<Professor?> GetProfessorByEmailAsync(String email);
+  Task<bool> AddProfessorAsync(Professor professor);
+  Task<bool> UpdateProfessorAsync(Professor professor);
+  Task<bool> DeleteProfessorByIdAsync(String id);
+  Task<bool> ProfessorExistsByIdAsync(String id);
+  Task<bool> ProfessorExistsByUsernameAsync(String username);
+  Task<bool> ProfessorExistsByEmailAsync(String email);
 }
