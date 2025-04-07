b8adf9639c89b8f436e74a3c026343a01e26b5c6
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-04-07 18:28:41 -0500

    Add unfinished methods to AuthService

---
+++ AttendanceSystem/Services/AuthService.cs
@@ -0,0 +1,15 @@
+using AttendanceSystem.Data.Repositories;
+using AttendanceSystem.Models;
+using AttendanceSystem.Models.DTOs;
+
+namespace AttendanceSystem.Services;
+
+public class AuthService : IAuthService {
+  public Task<AuthResponseDTO?> RegisterAsync(RegisterDTO registerDTO) {
+    
+  }
+
+  public Task<AuthResponseDTO?> LoginAsync(LoginDTO loginDTO) {
+    
+  }
+}
