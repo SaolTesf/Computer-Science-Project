8a6965e9d99f2feb23ad978ea8d34077e0214fcd
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-04-15 15:55:24 -0500

    Refactor HomePage and LoginPage to accept ClientService as a parameter for improved service management

--- ProfessorApp/Views/HomePage.xaml.cs
+++ ProfessorApp/Views/HomePage.xaml.cs
@@ -1,12 +1,20 @@
 ﻿using System.Text;
 using Newtonsoft.Json;
 using AttendanceShared.DTOs;
+using ProfessorApp.Services;
 
 
 namespace ProfessorApp.Pages
 {
     public partial class HomePage : ContentPage
     {
+        private readonly ClientService _clientService;
+
+        public HomePage(ClientService clientService)
+        {
+            InitializeComponent();
+            _clientService = clientService;
+        }
         private async void GoToSMPage(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync(nameof(StudentManagement));
